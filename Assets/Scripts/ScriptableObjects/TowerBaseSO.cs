74da0becaa32c4bf4eeca299605b63a4ed79e445
Benjamin Sims <asschaps@outlook.com>
2019-09-04 14:19:02 -0600

    Added SCriptableObjects to tower bases

---
+++ Assets/Scripts/ScriptableObjects/TowerBaseSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects {
+    [CreateAssetMenu(fileName = "New Base", menuName = "Base")]
+    public class TowerBaseSO : ScriptableObject {
+        public float towerMultiplier;
+    }
+}
---
+++ Assets/Scripts/TowerBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class TowerBase : MonoBehaviour {
+    public TowerBaseSO towerBase;
+    void Start () {
+        Debug.Log(towerBase.towerMultiplier);
+    }
+}
